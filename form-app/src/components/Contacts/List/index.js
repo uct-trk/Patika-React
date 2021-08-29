e1c399ae23af737d061c9be2741881d85e7a2141
Ugurcan Turk <ugurcan.uut82@gmail.com>
2021-08-29 18:12:29 +0300

    listeleme ve filtreleme işlemi, filter and list

--- form-app/src/components/Contacts/List/index.js
+++ form-app/src/components/Contacts/List/index.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const List = () => {
-    return (
-        <div>
-            List
-        </div>
-    )
-}
+const List = ({ contacts }) => {
+  const [filterText, setFilterText] = useState("");
 
-export default List
+  // contactları filtrelemek
+  const filtered = contacts.filter((item) => {
+    return Object.keys(item).some((key) => {
+      return item[key]
+        .toString()
+        .toLowerCase()
+        .includes(filterText.toLocaleLowerCase());
+    });
+  });
+  const filteredContact = (e) => {
+    setFilterText(e.target.value);
+  };
+  console.log(filtered);
+
+  return (
+    <div>
+      <input
+        value={filterText}
+        onChange={filteredContact}
+        placeholder="Filter Contact"
+      />
+      <ol>
+        {filtered.map((contact, index) => (
+          <li key={index}>{contact.fullname}</li>
+        ))}
+      </ol>
+    </div>
+  );
+};
+
+export default List;
--- form-app/src/components/Contacts/index.js
+++ form-app/src/components/Contacts/index.js
@@ -3,15 +3,28 @@ import Form from './Form'
 import List from './List'
 
 const Contacts = () => {
-    const [contacts, setContacts] = useState([])
+    const [contacts, setContacts] = useState([
+        {
+            fullname: "Uğurcan",
+            phone_number: "123123"
+        },
+        {
+            fullname: "Ahmet",
+            phone_number: "456546"
+        },
+        {
+            fullname: "Ayşe",
+            phone_number: "7878979"
+        },
+    ])
 
     useEffect(() => {
         console.log(contacts)
     }, [contacts])
     return (
         <div>
-            <List/>
             <Form setContacts={setContacts} contacts={contacts}/>
+            <List contacts={contacts}/>
         </div>
     )
 }
