9380f34fbcf326bdde9a04f7793653b79e767a95
Ugurcan Turk <ugurcan.uut82@gmail.com>
2021-09-11 14:10:18 +0300

    useMemo bizim istediğimiz durumda componentin render edilmesi veya edilmemesi

--- memoization/src/App.js
+++ memoization/src/App.js
@@ -1,20 +1,43 @@
-import react, { useState } from 'react'
+import { useState, useMemo } from 'react'
 import Header from './components/Header';
+
 function App() {
 
   const [number, setNumber] = useState(0)
+  const [text,setText] = useState("")
+
+  const data = useMemo(() => {
+    return calculate()
+  },[number])
+  
 
   const increase = () => {
     setNumber(number + 1)
   }
 
+  const handleChange = (e) => {
+    setText(e.target.value)
+  }
+  console.log(text)
+
   return (
     <div className="App">
-      <Header number={number > 5 ? number : 0}/>
+      <Header number={number > 5 ? number : 0} data={data}/>
       <h1>{number}</h1>
       <button onClick={increase}>+</button>
+      <br />
+      <br/>
+      <input value={text} onChange={handleChange} type="text" />
     </div>
   );
+}
+
+function calculate(){
+  console.log("Calculating...");
+  for(let i = 0; i < 1000; i++){}
+  console.log("calculating completed")
+
+  return {name : "ugurcan"}
 }
 
 export default App;
--- memoization/src/components/Header.js
+++ memoization/src/components/Header.js
@@ -1,14 +1,17 @@
 import React from 'react'
 
-const Header = ({number}) => {
+const Header = ({number, data}) => {
     console.log("render edilme durumu")
     return (
         <div>
             <h1>{number}</h1>
             Header
+            <br/>
+            <div>{JSON.stringify(data)}</div>
         </div>
     )
 }
 
 export default React.memo(Header)
 // React memo gereksiz renderlerin önüne geçebiliyor
+// React memoda componente gönderilen proplar değişmediği sürece render edilmez
